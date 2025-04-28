bb1cf7aeedc3d81e573f50000a6e157cf36c3b56
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-04-28 22:37:05 +0100

    ADD: undo movement
    To be used as a means of avoiding soft-locks

--- scenes/character/character_manager.gd
+++ scenes/character/character_manager.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+signal undo
+signal characters_moved
+
 @onready var characters: Array = get_tree().get_nodes_in_group("characters")
 
 
@@ -10,12 +13,18 @@ func _process(_delta: float) -> void:
 
 	if Input.is_action_pressed("character_up"):
 		move_characters(Vector2i.UP)
+		characters_moved.emit()
 	elif Input.is_action_pressed("character_down"):
 		move_characters(Vector2i.DOWN)
+		characters_moved.emit()
 	elif Input.is_action_pressed("character_left"):
 		move_characters(Vector2i.LEFT)
+		characters_moved.emit()
 	elif Input.is_action_pressed("character_right"):
 		move_characters(Vector2i.RIGHT)
+		characters_moved.emit()
+	elif Input.is_action_just_pressed("undo"):
+		undo.emit()
 
 
 func any_moving() -> bool:
