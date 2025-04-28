8ab8f434444bcf0cc9cd10408336f3cb8589e41a
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-04-28 16:02:33 +0100

    ADD: inter-character collision
    two characters can now share any given enclosed space. Expanded puzzle potential

--- scenes/character/components/grid_mover.gd
+++ scenes/character/components/grid_mover.gd
@@ -24,12 +24,20 @@ func _physics_process(_delta: float) -> void:
 func move(direction: Vector2i) -> void:
 	var current_tile: Vector2i = ground.local_to_map(get_parent().global_position)
 	var target_tile: Vector2i = current_tile + direction
-
-
 	var target_tile_data: TileData = walls.get_cell_tile_data(target_tile)
+
 
 	if target_tile_data != null and target_tile_data.get_custom_data('wall_obstacle'):
 		return
+
+	for character in get_tree().get_nodes_in_group("characters"):
+		if Vector2i(character.position) == (target_tile * 32 + Vector2i(16,16)):
+
+			var extended_target_tile: Vector2i = target_tile + direction
+			var extended_target_tile_data: TileData = walls.get_cell_tile_data(extended_target_tile)
+
+			if extended_target_tile_data != null and extended_target_tile_data.get_custom_data('wall_obstacle'):
+				return
 
 
 	is_moving = true
