6a65381709379013852f30619a44b415ca0a11f4
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-05-09 21:54:06 +0100

    ADD: level 5

--- scripts/globals/levels_data.gd
+++ scripts/globals/levels_data.gd
@@ -3,10 +3,10 @@ extends Node
 
 var level_unlocked_status := {
 	"Level_01": true,
-	"Level_02": false,
-	"Level_03": false,
-	"Level_04": false,
-	"Level_05": false,
+	"Level_02": true,
+	"Level_03": true,
+	"Level_04": true,
+	"Level_05": true,
 }
 
 var is_in_session := false
