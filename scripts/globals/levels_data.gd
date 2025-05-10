65e4821ace6687ce4334fe85f14b0f438528bf8a
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-05-10 18:09:37 +0100

    FIX: level select icons

--- scripts/globals/levels_data.gd
+++ scripts/globals/levels_data.gd
@@ -2,15 +2,16 @@ extends Node
 
 
 var level_unlocked_status := {
-	"Level_01": true,
-	"Level_02": true,
-	"Level_03": true,
-	"Level_04": true,
-	"Level_05": true,
-	"Level_06": true,
-	"Level_07": true,
-	"Level_08": true,
-	"Level_09": true,
+	"Level_01": false,
+	"Level_02": false,
+	"Level_03": false,
+	"Level_04": false,
+	"Level_05": false,
+	"Level_06": false,
+	"Level_07": false,
+	"Level_08": false,
+	"Level_09": false,
+	"Level_10": false,
 }
 
 var is_in_session := false
