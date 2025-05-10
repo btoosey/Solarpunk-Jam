22841d0c81e8c5bb3c5be92fa43662e594f28f75
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-05-10 23:02:30 +0100

    ADD: main theme

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,9 @@
 class_name Main
 extends Node2D
 
+
+@export var main_theme: AudioStream
+
 @onready var game_state_machine: GameStateMachine = $GameStateMachine as GameStateMachine
 @onready var level: Node2D = $Level
 
@@ -9,6 +12,7 @@ const CHARACTER_SIZE := Vector2(26, 36)
 
 func _ready() -> void:
 	game_state_machine.init()
+	MusicPlayer.play(main_theme)
 
 
 func _on_level_completed() -> void:
